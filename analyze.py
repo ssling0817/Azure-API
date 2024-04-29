586bb8a53ead43002149a3b66368cf49ca306ccd
ssling0817 <70581390+ssling0817@users.noreply.github.com>
2024-04-29 14:07:21 -0400

    Add files via upload

---
+++ analyze.py
@@ -0,0 +1,53 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+import time
+import os
+
+
+endpoint = "https://mlip-lab1-00.cognitiveservices.azure.com/"
+# key = "89ee07d8bfd3481e8eae2191d16fe07d"
+
+
+key = os.getenv("key")
+credentials = CognitiveServicesCredentials(key)
+
+client = ComputerVisionClient(
+    endpoint=endpoint,
+    credentials=credentials
+)
+
+def read_image(uri):
+    numberOfCharsInOperationId = 36
+    maxRetries = 10
+
+    # SDK call
+    rawHttpResponse = client.read(uri, language="en", raw=True)
+
+    # Get ID from returned headers
+    operationLocation = rawHttpResponse.headers["Operation-Location"]
+    idLocation = len(operationLocation) - numberOfCharsInOperationId
+    operationId = operationLocation[idLocation:]
+
+    # SDK call
+    result = client.get_read_result(operationId)
+    
+    # Try API
+    retry = 0
+    
+    while retry < maxRetries:
+        if result.status.lower () not in ['notstarted', 'running']:
+            break
+        time.sleep(1)
+        result = client.get_read_result(operationId)
+        
+        retry += 1
+    
+    if retry == maxRetries:
+        return "max retries reached"
+
+    if result.status == OperationStatusCodes.succeeded:
+        res_text = " ".join([line.text for line in result.analyze_result.read_results[0].lines])
+        return res_text
+    else:
+        return "error"
